e1e204ee29569b730601d0c358226b783cde8554
CasanovaRamiro <casanovaramiroj@gmail.com>
2026-05-16 19:56:48 -0300

    rompo mig

--- server/prisma/migrations/20260419175854_init_user_table/migration.sql
+++ server/prisma/migrations/20260419175854_init_user_table/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL unique,
     `password` VARCHAR(191) NOT NULL,
     `phone` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
