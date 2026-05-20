3f7430ecf863509f61f514b8419dd3803e8c8c66
Kvnrot-sys <kvnrotela@gmail.com>
2026-05-20 15:14:47 -0300

    commit 1 - revisar test
    creacion de modelos de publicacion. seguir corroborando test

---
+++ server/prisma/migrations/20260520021344_remove_photo_from_publication/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `photo` on the `publication` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `publication` DROP COLUMN `photo`;
