efc03a925e968ac67fb058c21e477ba18e7fac2d
Martin <ursomartin89@gmail.com>
2026-05-25 14:02:37 -0300

    Solucion de conficto de mergeo 2

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     include: ['test/**/*.test.ts'],
     environment: 'node',
    env: {
-    DATABASE_URL: 'mysql://root:root@localhost:3306/homefix_db',
+    DATABASE_URL: 'mysql://ofix:ofix123@localhost:3306/ofix_test',
     JWT_SECRET: 'test-secret',
     AUTH0_AUDIENCE: 'https://api.miapinode.com',
     AUTH0_ISSUER_BASE_URL: 'https://dev-thdx752hxc0ircbe.us.auth0.com/',
