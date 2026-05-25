6c4d695cc9d2fada3bd3855c59b0159080f38210
EricNahuel2002 <ericaquino2002@gmail.com>
2026-05-25 17:10:38 -0300

    Agregada la vista de post sin ia, agregada ruta de categoria para obtenerlo en el front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './views/Login'
 import Register from './views/Register'
 import Users from './views/Users'
-import Diagnostico from './views/Diagnostico'
+import Diagnostico from './views/AiDiagnosis'
+import CrearPublicacion from './views/CreatePost'
 
 const PrivateRoute = ({ children }: { children: ReactNode }) =>
   localStorage.getItem('token') ? children : <Navigate to="/login" replace />
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/users" element={<PrivateRoute><Users /></PrivateRoute>} />
         <Route path="/diagnostico" element={<PrivateRoute><Diagnostico /></PrivateRoute>} />
+        <Route path="/crear-publicacion" element={<PrivateRoute><CrearPublicacion /></PrivateRoute>} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
