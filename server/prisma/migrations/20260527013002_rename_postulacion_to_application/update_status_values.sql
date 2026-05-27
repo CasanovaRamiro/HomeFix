1861d0e7f9665992d3281f36580a9fe678bd3d83
Gian Franco Pretto Paseiro <gianpretto@hotmail.com>
2026-05-27 19:49:39 -0300

    refactor: remover redundantes de la codebase

--- server/prisma/migrations/20260527013002_rename_postulacion_to_application/update_status_values.sql
+++
@@ -1,4 +0,0 @@
--- Update existing status values from Spanish to English
-UPDATE `Application` SET `status` = 'Accepted'  WHERE `status` = 'Aceptada';
-UPDATE `Application` SET `status` = 'Rejected'  WHERE `status` = 'Rechazada';
-UPDATE `Application` SET `status` = 'Pending'   WHERE `status` = 'Pendiente';
