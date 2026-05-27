8b99d325a805103236f9515f1c7a422b504e0689
CasanovaRamiro <casanovaramiroj@gmail.com>
2026-05-27 20:53:44 -0300

    server logs

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ validateEnv()
 
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 import workerRoutes from './routes/worker.routes.js'
@@ -19,6 +20,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }))
 app.use('/auth', authRoutes)
