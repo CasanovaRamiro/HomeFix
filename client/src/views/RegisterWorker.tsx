cdc6791f8aa3ad747eadb8898ce8fd759101d640
MartinMeza22 <martinmeza.t22@gmail.com>
2026-06-18 00:31:27 -0300

    Revert "fix: CI"
    This reverts commit 9f11b4b2b7d52f8524ce25469a833ce13e435eb7.

--- client/src/views/RegisterWorker.tsx
+++ client/src/views/RegisterWorker.tsx
@@ -4,7 +4,7 @@ import type { ChangeEvent, FormEvent } from 'react'
 import {
   Eye, EyeOff, Mail, Lock, User, Phone, Briefcase,
   AlertCircle, ArrowRight, ArrowLeft, Shield, Check,
-  CreditCard, MailCheck,
+  CreditCard, MailCheck, Fingerprint,
 } from 'lucide-react'
 import api from '../services/api'
 import { useTheme } from '../hooks/useTheme'
